a5e2b1dea1b12285d0956f43256492c5783d899b
CyrilPERIE <cyril.perie96@gmail.com>
2024-06-21 08:37:17 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,18 @@ import { Admin } from "./components/pages/Admin";
 
 function App() {
 
-  const isConnected = false
+  const isConnected = true
 
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/login" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Navigate replace to="/dashboard" /> : <Login />} />
-          <Route path="/dashboard" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Dashboard /> : <Navigate replace to="/login" />} />
-          <Route path="/report" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Report /> : <Navigate replace to="/login" />} />
-          <Route path="/stats" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Stats /> : <Navigate replace to="/login" />} />
-          <Route path="/admin" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Admin /> : <Navigate replace to="/login" />} />
-          <Route path="/" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Navigate replace to="/dashboard" /> : <Navigate replace to="/login" />} />
-          <Route path="*" element={useSelector((state: RootState) => state.user.user.isConnected) ? <Navigate replace to="/dashboard" /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={isConnected ? <Navigate replace to="/dashboard" /> : <Login />} />
+          <Route path="/dashboard" element={isConnected ? <Dashboard /> : <Navigate replace to="/login" />} />
+          <Route path="/report" element={isConnected ? <Report /> : <Navigate replace to="/login" />} />
+          <Route path="/stats" element={isConnected ? <Stats /> : <Navigate replace to="/login" />} />
+          <Route path="/admin" element={isConnected ? <Admin /> : <Navigate replace to="/login" />} />
+          <Route path="/" element={isConnected ? <Navigate replace to="/dashboard" /> : <Navigate replace to="/login" />} />
+          <Route path="*" element={isConnected ? <Navigate replace to="/dashboard" /> : <Navigate replace to="/login" />} />
         </Routes>
     </BrowserRouter>
   );
