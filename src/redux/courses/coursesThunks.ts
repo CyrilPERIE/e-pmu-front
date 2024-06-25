47d1e90d3dfb054277977b077e68ed9d1d88d9cd
Cyril PERIE <cyril.perie96@gmail.com>
2024-06-25 18:23:05 +0200

    Enable connection with NAS

--- src/redux/courses/coursesThunks.ts
+++ src/redux/courses/coursesThunks.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export const fetchCourses = createAsyncThunk(
   'courses/fetchCourses',
   async (reunion_id: number) => {
-    const response = await fetch(`http://localhost:8081/programme/${reunion_id}/courses`);
+    const response = await fetch(`http://${ process.env.REACT_APP_HOST || 'localhost'}:8081/programme/${reunion_id}/courses`);
     const data = await response.json();
     return data;
   }
--- src/redux/participants/participantsThunks.ts
+++ src/redux/participants/participantsThunks.ts
@@ -3,7 +3,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export const fetchParticipants = createAsyncThunk(
   'participants/fetchParticipants',
   async (course: number) => {
-    const response = await fetch(`http://localhost:8081/programme/${course}/participants`);
+    const response = await fetch(`http://${process.env.REACT_APP_HOST ||'localhost'}:8081/programme/${course}/participants`);
     const data = await response.json();
     return data;
   }
